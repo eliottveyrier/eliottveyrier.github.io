458a56c2645be26d351bed2d596c0535421bbca3
Eliott Veyrier <eliottveyrier00@gmail.com>
2026-07-20 15:32:31 +0200

    fix: move content config back to src

--- src/content.config.ts
+++ src/content.config.ts
@@ -20,4 +20,19 @@ const blog = defineCollection({
     }),
 });
 
-export const collections = { blog };
+const music = defineCollection({
+    loader: glob({
+        pattern: "**/*.{md,mdx}",
+        base: "./src/content/music",
+    }),
+
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        image: z.string(),
+        year: z.number().optional(),
+        priority : z.number().optional(),
+    }),
+});
+
+export const collections = { blog, music };
