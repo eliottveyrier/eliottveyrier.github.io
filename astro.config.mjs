99df1c157db69a86f90077c3652a0c695d01fc12
Eliott Veyrier <eliottveyrier00@gmail.com>
2026-07-21 13:17:26 +0200

    Add astro-expressive-code

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import mdx from '@astrojs/mdx';
 
 import preact from '@astrojs/preact';
 
+import expressiveCode from 'astro-expressive-code';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), preact()],
+  integrations: [mdx(), preact(), expressiveCode()],
   site: 'https://eliottveyrier.github.io',
   base: '/'
 });
