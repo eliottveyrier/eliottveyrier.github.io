a406771e8f00fcbea91c27d137083ddd84ec1ace
Eliott Veyrier <eliottveyrier00@gmail.com>
2026-07-21 13:18:40 +0200

    move expressiveCode before mdx as required

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import expressiveCode from 'astro-expressive-code';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), preact(), expressiveCode()],
+  integrations: [expressiveCode(), mdx(), preact() ],
   site: 'https://eliottveyrier.github.io',
   base: '/'
 });
