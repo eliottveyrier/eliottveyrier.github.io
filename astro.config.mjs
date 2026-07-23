95c5f484bf77c4029183553b9d14d76e517aca1c
Eliott Veyrier <eliottveyrier00@gmail.com>
2026-07-23 09:44:09 +0200

    add astro icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,14 +7,15 @@ import preact from '@astrojs/preact';
 
 import expressiveCode from 'astro-expressive-code';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    expressiveCode({
-      // Replace the default themes with a custom set of bundled themes:
-      // "dracula" (a dark theme) and "solarized-light"
-      themes: ['dracula', 'solarized-light'],
-    }), mdx(), preact() ],
+  integrations: [expressiveCode({
+    // Replace the default themes with a custom set of bundled themes:
+    // "dracula" (a dark theme) and "solarized-light"
+    themes: ['dracula', 'solarized-light'],
+  }), mdx(), preact(), icon()],
   site: 'https://eliottveyrier.github.io',
   base: '/'
 });
